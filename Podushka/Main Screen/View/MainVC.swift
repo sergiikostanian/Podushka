dd4efff72e19e77ffd685e1dbc010c31f141af5d
Sergii Kostanian <sk@madappgang.com>
2020-05-09 09:53:42 +0300

    Add sleep timer value selection

--- Podushka/Main Screen/View/MainVC.swift
+++ Podushka/Main Screen/View/MainVC.swift
@@ -10,17 +10,25 @@ import UIKit
 
 final class MainVC: UIViewController {
 
+    // MARK: Outlets
     @IBOutlet private weak var stateLabel: UILabel!
     @IBOutlet private weak var sleepTimerValueLabel: UILabel!
     @IBOutlet private weak var alarmValueLabel: UILabel!
     @IBOutlet private weak var playPauseButton: UIButton!
+    
+    /// Measured in minutes.
+    private let availableSleepTimerValues = [0, 1, 3, 5, 10, 15]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - Actions
     @IBAction private func sleepTimerViewTapped(_ sender: UITapGestureRecognizer) {
+        selectSleepTimerValue { value in
+            // TODO: update value
+        }
     }
     
     @IBAction private func alarmViewTapped(_ sender: UITapGestureRecognizer) {
@@ -29,5 +37,22 @@ final class MainVC: UIViewController {
     @IBAction private func playPauseButtonTapped(_ sender: UIButton) {
     }
     
+    // MARK: - Helpers
+    private func selectSleepTimerValue(_ completion: @escaping (Int?) -> Void) {
+        let alert = UIAlertController(title: "Sleep Timer", message: nil, preferredStyle: .actionSheet)
+        
+        availableSleepTimerValues.forEach({ (value) in
+            let title = value > 0 ? "\(value) min" : "off"
+            alert.addAction(UIAlertAction(title: title, style: .default) { _ in
+                completion(value)
+            })
+        })
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in 
+            completion(nil)
+        })
+        
+        present(alert, animated: true)
+    }
 }
 
